fad7cb91a0d653a8fa54a17de6da3b0953729869
SafLabs <alsafwan.abdullah@gmail.com>
2021-01-28 07:51:49 +0300

    23/1/2021

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
         <div className={"ui secondary pointing menu"}>
             <Link to={"/"} className={"item"}>Streamer</Link>
             <div className={"right menu"}>
+                <GoogleAuth/>
                 <Link className={"item"} to={"/"} >All Streams</Link>
                 <Link className={"item"} to={"/streams/new"} >Create</Link>
-                <GoogleAuth/>
             </div>
         </div>
     );
